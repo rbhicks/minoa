630c75158d46ecd7b712dc3e769a1b91cf52a7a6
rbhicks <r.b.hicks@gmail.com>
2020-06-06 20:29:42 +0200

    Added grid generation with randomly placed hero.

--- lib/minoa_web/live/maze.ex
+++ lib/minoa_web/live/maze.ex
@@ -6,117 +6,24 @@ defmodule MinoaWeb.Maze do
   end
 
   def render(assigns) do
+    hero_square = Enum.random(1..100)
     ~L"""
     <main class="maze">
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
-      <b class="closed-square"></b>
+    <%= for index <- 1..100 do %>
+      <%= if Enum.random(0..1) == 1  do %>
+        <div class="closed-square">
+          <%= unless index != hero_square do %>
+            <div class="hero"></div>
+          <% end %>
+        </div>
+      <% else %>
+        <div class="open-square">
+          <%= unless index != hero_square do %>
+            <div class="hero"></div>
+          <% end %>
+        </div>
+      <% end %>
+    <% end %>
     </main>
     """
   end
