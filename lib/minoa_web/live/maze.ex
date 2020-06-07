4070bc1b8b885771e47980deeaedb3ad7f6c25de
rbhicks <r.b.hicks@gmail.com>
2020-06-07 05:27:59 +0200

    changed handling to use maze from the maze server.

--- lib/minoa_web/live/maze.ex
+++ lib/minoa_web/live/maze.ex
@@ -2,26 +2,16 @@ defmodule MinoaWeb.Maze do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :maze, [])}
+    {:ok, assign(socket, :maze, GenServer.call(:maze_server, :get_maze))}
   end
 
   def render(assigns) do
-    hero_square = Enum.random(1..100)
     ~L"""
     <main>
     <section class="maze">
-    <%= for index <- 1..100 do %>
-      <%= if Enum.random(0..1) == 1  do %>
-        <div class="closed-square">
-          <%= unless index != hero_square do %>
-            <div class="hero"></div>
-          <% end %>
-        </div>
-      <% else %>
-        <div class="open-square">
-          <%= unless index != hero_square do %>
-            <div class="hero"></div>
-          <% end %>
+    <%= for i <- 0..9 do %>
+      <%= for j <- 0..9 do %>
+        <div class="<%= @maze[i][j] %>">
         </div>
       <% end %>
     <% end %>
