36853b51025228337419d232fc3615b3981aee04
rbhicks <r.b.hicks@gmail.com>
2020-06-08 13:52:13 +0200

    Initialized nil pid in assigns during mount to set up pattern matching for start handlers. Added class selection function. Change redering to use class selection function.

--- lib/minoa_web/live/game.ex
+++ lib/minoa_web/live/game.ex
@@ -5,39 +5,38 @@ defmodule MinoaWeb.Game do
   def mount(_params, _session, socket) do
     {:ok, assign(socket,
                  maze: GenServer.call(:maze_server, :get_maze),
-                 player_id: UUID.uuid1())}
-  end
-
-  def handle_event(
-        "start",
-        %{"player_id" => _player_id,
-          "player_name" => _player_name},
-        %Phoenix.LiveView.Socket{assigns: %{pid: pid}}=socket) do
-    GenServer.call(pid, :place_player_randomly)
-    {:noreply, assign(socket, pid: pid, maze: GenServer.call(:maze_server, :get_maze))}
+                 player_id: UUID.uuid1(),
+                 pid: nil)}
   end
 
   def handle_event(
         "start",
         %{"player_id" => player_id},
-        socket) do
+        %Phoenix.LiveView.Socket{assigns: %{pid: nil}}=socket) do
     {:ok, pid} = Minoa.PlayerSupervisor.start_player(player_id)
     GenServer.call(pid, :place_player_randomly)
     {:noreply, assign(socket, pid: pid, maze: GenServer.call(:maze_server, :get_maze))}
   end
 
+  def handle_event(
+        "start",
+        _,
+        %Phoenix.LiveView.Socket{assigns: %{pid: pid}}=socket) do
+    GenServer.call(pid, :place_player_randomly)
+    {:noreply, assign(socket, maze: GenServer.call(:maze_server, :get_maze))}
+  end
+  
   def handle_event("left", _params, socket) do
     {:noreply, socket}
   end
   
-
   def render(assigns) do
     ~L"""
     <main>
     <section class="maze">
     <%= for x <- 0..9 do %>
       <%= for y <- 0..9 do %>
-        <div class="<%= @maze[x][y] %>">
+        <div class="<%= get_square_class(@pid, @maze[x][y]) %>">
         </div>
       <% end %>
     <% end %>
@@ -65,4 +64,15 @@ defmodule MinoaWeb.Game do
     </main>
     """
   end
+
+  defp get_square_class(player_pid, class_determinant) when is_pid(class_determinant) do
+    if(player_pid == class_determinant) do
+      "hero"
+    else
+      "enemy"
+    end
+  end
+
+  defp get_square_class(nil, class_determinant), do: class_determinant
+  defp get_square_class(_pid, class_determinant), do: class_determinant
 end
